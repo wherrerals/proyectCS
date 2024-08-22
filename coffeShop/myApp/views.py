4724963062cfd9e33a4c75108ed62f9af2b9afcd
wherrerals <wherrera@ledstudio.cl>
2024-08-22 18:27:28 -0400

    corrigiendo novedad de productos en myApp

--- coffeShop/myApp/views.py
+++ coffeShop/myApp/views.py
@@ -3,5 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 def listadoProductos(request):
+
+    list_productos = [
+        {'nombre': 'Producto 1', 'precio': 1000},
+        {'nombre': 'Producto 2', 'precio': 2000},
+        {'nombre': 'Producto 3', 'precio': 3000},
+        {'nombre': 'Producto 4', 'precio': 4000}
+    ]
+
+    context = {
+        'list_productos': list_productos
+    }
     
-    return render(request, 'myApp/producto.html')
+    return render(request, 'myApp/producto.html', context)
